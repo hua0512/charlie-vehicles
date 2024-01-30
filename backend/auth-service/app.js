e40bb16b4ff78b6a6d78a537f1ec77bfdc4c297a
hua0512 <whua0512@gmail.com>
2024-01-30 14:37:54 +0100

    Change to use HS256 JWT

--- backend/auth-service/app.js
+++ backend/auth-service/app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
-app.use('/login', cors(), loginRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
